867bb592df250b8f7769bacd2dc1beac5d6729d5
ianertson <sebbekarlsson97@gmail.com>
2018-08-30 14:19:06 +0200

    added all VBScript types

--- src/includes/TOKEN_TYPES.h
+++ src/includes/TOKEN_TYPES.h
@@ -5,5 +5,16 @@
 
 std::string T_INTEGER = "INTEGER";
 std::string T_PLUS = "PLUS";
+std::string T_BOOLEAN = "BOOLEAN";
+std::string T_BYTE = "BYTE";
+std::string T_LONG = "LONG";
+std::string T_SINGLE = "SINGLE";
+std::string T_DOUBLE = "DOUBLE";
+std::string T_DATE = "DATE";
+std::string T_STRING = "STRING";
+std::string T_OBJECT = "OBJECT";
+std::string T_ARRAY = "ARRAY";
+std::string T_EMPTY = "EMPTY";
+std::string T_NULL = "NULL";
 std::string T_EOF = "EOF";
 #endif
