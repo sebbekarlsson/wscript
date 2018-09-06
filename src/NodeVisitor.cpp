7137b6d2f90feaffbef4d08d45ff6be850233bb5
sebastian karlsson <sebbekarlsson97@gmail.com>
2018-09-06 15:24:02 +0200

    added comments about how to write the code in NodeVisitor

--- src/NodeVisitor.cpp
+++ src/NodeVisitor.cpp
@@ -2,9 +2,17 @@
 
 
 std::string NodeVisitor::visit(AST* node) {
+    // if (node->type == BinOp) {
+    //     return this-visit_BinOp(node);
+    // } else if (node->type == Num) {
+    //     returh this->visit_Num(node);
+    // } else {
+    //     return this->visit_default(node);
+    // }
     return "";
 };
 
 std::string NodeVisitor::visit_default(AST* node) {
+    // TODO: Throw error here
     return "";
 };
