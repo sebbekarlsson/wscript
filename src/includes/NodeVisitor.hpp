9725c6509e4d4eccf5b572f31fc7043408eb7528
sebastian karlsson <sebbekarlsson97@gmail.com>
2018-09-06 15:20:59 +0200

    added virtual methods to NodeVisitor

--- src/includes/NodeVisitor.hpp
+++ src/includes/NodeVisitor.hpp
@@ -8,5 +8,8 @@ class NodeVisitor {
     public:
         std::string visit(AST* node);
         std::string visit_default(AST* node);
+
+        virtual std::string visit_BinOp(AST* node) = 0;
+        virtual std::string visit_Num(AST* node) = 0;
 };
 #endif
