a10e26e1251928500d5b060ab0b149cc72532359
ianertson <sebbekarlsson97@gmail.com>
2018-10-13 17:01:07 +0200

    delete noop private scope

--- src/AST/AST_NoOp.cpp
+++ src/AST/AST_NoOp.cpp
@@ -1,6 +1,10 @@
 #include "../includes/AST/AST_NoOp.hpp"
+#include "../includes/Scope.hpp"
 
 
-AST_NoOp::AST_NoOp() {};
+AST_NoOp::AST_NoOp() {
+    delete this->private_scope;
+    this->private_scope = nullptr;
+};
 
 AST_NoOp::~AST_NoOp() {};
